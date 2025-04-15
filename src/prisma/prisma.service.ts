de20ceccd6b1eedbbabe25b45109c14050a99cc9
GunjanBidpath <g.sheth@bidpath.com>
2025-04-15 13:06:09 +0530

    final changes to make running on dockerized environment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,10 +3,21 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL, // 👈 force it to use this
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
 
+  // If you want graceful shutdowns:
   // async enableShutdownHooks(app: INestApplication) {
   //   this.$on('beforeExit' as any, async () => {
   //     await app.close();
